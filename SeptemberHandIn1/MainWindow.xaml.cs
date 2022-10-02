340b1e25cd1dbb23680f28994c5d2f88db3b2d36
riccardihithub <101105168+riccardihithub@users.noreply.github.com>
2022-10-02 19:14:35 +0200

    Initial commit

---
+++ SeptemberHandIn1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextClassification.Controller;
+using TextClassification.Domain;
+
+namespace SeptemberHandIn1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BagOfWords bow;
+        private KnowledgeBuilder kb;
+        private List<string> entries;
+        private Knowledge knowledge;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            kb = new KnowledgeBuilder();
+           kb.Train();
+        }
+    }
+}
